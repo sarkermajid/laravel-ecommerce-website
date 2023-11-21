b23195747cb8b144aacca203b7ad391f761fcf74
sarkermajid <majidursarkar333@gmail.com>
2023-11-21 23:52:53 +0600

    general settings controller, model and database create

---
+++ database/migrations/2023_11_21_173939_create_general_settings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('general_settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('header_logo')->nullable();
+            $table->string('header_message')->nullable();
+            $table->string('shop_address')->nullable();
+            $table->string('shop_phone')->nullable();
+            $table->string('shop_email')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('footer_logo')->nullable();
+            $table->string('facebook_link')->nullable();
+            $table->string('instagram_link')->nullable();
+            $table->string('twitter_link')->nullable();
+            $table->string('linkedin_link')->nullable();
+            $table->string('open_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('general_settings');
+    }
+};
